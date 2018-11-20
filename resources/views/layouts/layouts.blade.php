d5d9c2b785cfc085b35f7b8750c8a7a270a66e63
Michael <your.email@example.com>
2018-11-20 18:32:24 +0300

    change for asset() to secure_asset()

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,24 +3,24 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="log3.png" href="{{ asset ('img/log3.png') }}">
+<link rel="log3.png" href="{{ secure_asset ('img/log3.png') }}">
 <title>Nairobi East Sda Church</title>
 <meta name="description" content="">
 <meta name="author" content="">
 
 <!-- Favicon
     ================================================== -->
-<link rel="shortcut icon" href="{{ asset('img/logo2.png') }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ secure_asset('img/logo2.png') }}" type="image/x-icon">
 
 
 <!-- Bootstrap -->
-<link rel="stylesheet" type="text/css"  href="{{ asset ('css/bootstrap.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset ('fonts/font-awesome/css/font-awesome.css') }}">
+<link rel="stylesheet" type="text/css"  href="{{ secure_asset ('css/bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ secure_asset ('fonts/font-awesome/css/font-awesome.css') }}">
 
 <!-- Stylesheet
     ================================================== -->
-<link rel="stylesheet" type="text/css"  href="{{ asset('css/style.css') }}">
-<link rel="stylesheet" type="text/css" href=" {{ asset ('css/animate.css') }}">
+<link rel="stylesheet" type="text/css"  href="{{ secure_asset('css/style.css') }}">
+<link rel="stylesheet" type="text/css" href=" {{ secure_asset ('css/animate.css') }}">
 <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="{{ asset ('js/modernizr.custom.js') }}"></script>
@@ -87,14 +87,14 @@ function closeNav() {
 new WOW().init(); 
 
 });</script>
-<script src="{{ asset ('js/wow.min.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/bootstrap.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/SmoothScroll.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/jquery.prettyPhoto.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/jquery.isotope.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/jqBootstrapValidation.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/contact_me.js') }}"></script> 
-<script type="text/javascript" src="{{ asset ('js/main.js') }}"></script>
+<script src="{{ secure_asset ('js/wow.min.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/bootstrap.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/SmoothScroll.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/jquery.prettyPhoto.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/jquery.isotope.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/jqBootstrapValidation.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/contact_me.js') }}"></script> 
+<script type="text/javascript" src="{{ secure_asset ('js/main.js') }}"></script>
 
 </body>
 </html>
